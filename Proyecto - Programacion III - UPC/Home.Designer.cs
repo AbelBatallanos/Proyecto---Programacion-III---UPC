2a8372e061a22ffc4e01270d3b04c359ac96fefb
EliasVRoca Laptop <eliasvroca@gmail.com>
2024-06-20 14:14:52 -0400

    Agregar archivos de proyecto.

---
+++ Proyecto - Programacion III - UPC/Home.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace Proyecto___Programacion_III___UPC
+{
+    partial class Home
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Cascadia Code", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(221, 198);
+            label1.Name = "label1";
+            label1.Size = new Size(336, 27);
+            label1.TabIndex = 0;
+            label1.Text = "PROYECTO - PROGRAMACIÓN III";
+            label1.Click += label1_Click;
+            // 
+            // Home
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label1);
+            Name = "Home";
+            Text = "Home";
+            Load += Home_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+    }
+}
---
+++ Proyecto - Programacion III - UPC/Home.cs
@@ -0,0 +1,18 @@
+namespace Proyecto___Programacion_III___UPC
+{
+    public partial class Home : Form
+    {
+        public Home()
+        {
+            InitializeComponent();
+        }
+        private void Home_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
